a74ea85ebbb7704f30daf633510bba5d5e0096d1
Sagar Pawar <pawar.sagar1990@gmail.com>
2018-08-02 00:47:04 +0530

    Forgot password

---
+++ ArtCrestApplication/ArtCrestApplication/forgotpassword.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Main.Master" AutoEventWireup="true" CodeBehind="forgotpassword.aspx.cs" Inherits="ArtCrestApplication.forgotpassword" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <!--User Verification Form-->
+    <div><asp:Label ID="lblErrorMsg" runat="server"></asp:Label><br /></div>
+	<section class="ver-sec">
+		<div class="container">
+			<div class="login-box">                
+				<label><b>Email ID</b></label>
+                <asp:TextBox runat="server" class="login-input" placeholder="Enter EmailID" ID="txtEmailID" name="txtEmailID" required />								
+                <asp:Button Text="Send Verfication Link" class="ver-link-btn" runat="server" ID="btnSendVerificationLink" OnClick="btnSendVerificationLink_Click" />
+			</div>
+		</div>
+	</section>
+	
+	<section class="chg-pwd-sec" runat="server" id="changePasswordDiv">
+		<div class="container">
+			<div class="chg-pwd-title">
+				<span>Change Password</span>
+			</div>
+			<div class="login-box">
+			<label for="uname"><b>New Password</b></label>
+			<input class="login-input" type="password" placeholder="Enter New Password" name="uname" required>
+
+			<label for="psw"><b>Retype Password</b></label>
+			<input class="login-input" type="password" placeholder="Retype Password" name="psw" required>
+        
+			<button class="login-btn" type="submit">Change Password</button>
+			</div>
+		</div>
+	
+	</section>
+</asp:Content>
---
+++ ArtCrestApplication/ArtCrestApplication/forgotpassword.aspx.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BusinessLayer;
+using DataAccessLayer;
+
+namespace ArtCrestApplication
+{
+    public partial class forgotpassword : System.Web.UI.Page
+    {
+        BusinessLayer.BusinessLayer objBusinessL = new BusinessLayer.BusinessLayer();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            changePasswordDiv.Visible = false;
+        }
+        protected void btnSendVerificationLink_Click(object sender, EventArgs e)
+        {
+            lblErrorMsg.Text = "";
+            try
+            {
+                if (txtEmailID.Text != string.Empty)
+                {
+                    Dictionary<string, string> parameters = new Dictionary<string, string>();
+                    parameters.Add("emailid", txtEmailID.Text);                    
+                    parameters.Add("isActve", "true");
+                    string query = "select * from users where useremailID = @emailid and isActive = @isActve;";
+                    DataTable dtUserDetail = DataAccessLayer.DataAccessLayer.getDataFromQueryWithParameters(query, parameters);
+                    if (dtUserDetail != null & dtUserDetail.Rows.Count > 0)
+                    {
+                        Guid guidResetLink = Guid.NewGuid();
+                        string userGuid = Convert.ToString(guidResetLink.ToString());                                                                     
+                        Dictionary<string, string> parametersLink = new Dictionary<string, string>();
+                        parametersLink.Add("linkGUID", userGuid);
+                        parametersLink.Add("lUserID", Convert.ToString(dtUserDetail.Rows[0]["UserID"]));
+                        parametersLink.Add("lIsActive", true.ToString());
+                        parametersLink.Add("lcreatedDate", DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss"));
+
+
+                        string insertQuery = "INSERT INTO [dbo].[forgotpasswordlink]([linkGUID],[fkUserID],[isActive],[CreatedDate])" +
+                                            "VALUES(@linkGUID,@lUserID,@lIsActive,@lcreatedDate);";
+
+                        int insertResult = DataAccessLayer.DataAccessLayer.ExecuteNonQuery(insertQuery, parametersLink);
+                        if (insertResult > 0)
+                        {
+                            objBusinessL.SendMail(txtEmailID.Text, "Skartif - Password Reset Link", "Please click on this link to reset your password. Link - www.skartif.com/forgotpassword.aspx?uid="+ userGuid);
+                            ShowErrorMsg("Please check your email, we have sent a link on your EmailID-"+txtEmailID.Text, false);
+                        }
+                        else
+                        {
+                            ShowErrorMsg("Oops! looks like there is some error while processing. Please try again.", true);
+                        }
+                    }
+                    else
+                    {
+                        ShowErrorMsg("Email ID does not exists in our system. Please enter valid Email ID.", true);
+                    }
+                }
+                else
+                {
+                    ShowErrorMsg("Please enter required fields", true);
+                }
+            }
+            catch (Exception ex)
+            {
+                ShowErrorMsg(ex.Message, true);
+                BusinessLayer.BusinessLayer.LogTracer(ex.Message + "- stack trace =" + ex.StackTrace.ToString(), "forgotpassword", "E", "admin");
+            }
+        }
+        public void ShowErrorMsg(string msg, bool isError)
+        {
+            lblErrorMsg.Text = msg;
+            lblErrorMsg.Font.Bold = true;
+            lblErrorMsg.Visible = true;
+            if (isError)
+            {
+                lblErrorMsg.ForeColor = System.Drawing.Color.Red;
+            }
+            else
+            {
+                lblErrorMsg.ForeColor = System.Drawing.Color.Green;
+            }
+        }
+    }
+}
